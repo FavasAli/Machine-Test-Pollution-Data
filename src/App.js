21e9fb1902009bc6071a97af10e20c7f4f387d2a
Favasdns <favas.a@deepnetsoft.com>
2022-10-03 10:40:09 +0530

    final submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import  Chart  from "./Chart/Chart"
+import Gg from "./Chart/Gg"
+import Hh from "./Chart/Hh"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Chart />
+
+    </>
+  )
 }
 
-export default App;
+export default App
